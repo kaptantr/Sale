66cdccc56b1cafc72d74d8ffa8b1ccb4c88c6286
Mesut EROL <mesut_erol_tr@hotmail.com>
2024-06-21 22:30:19 -0400

    Add project files.

---
+++ FistikSale/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+
+using FistikSale.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FistikSale.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ FistikSale/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FistikSale.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
